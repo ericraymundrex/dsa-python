5bb3fe24b8b1a59dca95aabf41fd05300f034d57
Eric Raymund Rex Sylvester <58367989+ericraymundrex@users.noreply.github.com>
2024-02-17 18:45:06 +0530

    added classes -1

---
+++ 1-classes-and-pointers/1_classes.py
@@ -0,0 +1,11 @@
+class Cokies:
+	def __init__(self,color):
+		self.color=color
+	def print_color(self):
+		print(self.color)
+
+g=Cokies('green')
+g.print_color()
+
+b=Cokies('blue')
+b.print_color()
